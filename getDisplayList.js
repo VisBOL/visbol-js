c8fbff615a541fbfc203fefda66678d894d2bbe3
Zach Zundel <zach.zundel@utah.edu>
2017-05-01 05:03:21 -0600

    Config support

--- getDisplayList.js
+++ getDisplayList.js
@@ -5,90 +5,96 @@ var sbolmeta = require('sbolmeta')
 
 var URI = require('urijs')
 
-function getDisplayList(componentDefinition) {
+function getDisplayList(componentDefinition, config) {
 
     var segments = [
-        getDisplayListSegment(componentDefinition)
+        getDisplayListSegment(componentDefinition, config)
     ]
 
-    segments = recurseGetDisplayList(componentDefinition,segments)
+    segments = recurseGetDisplayList(componentDefinition, segments, config)
 
     return segments
 
 }
 
-function recurseGetDisplayList(componentDefinition,segments) {
+function recurseGetDisplayList(componentDefinition, segments, config) {
 
-    if(componentDefinition.visited)
+    if (componentDefinition.visited)
         return []
 
     componentDefinition.visited = true
 
     sortedSubComponents(componentDefinition).forEach((component) => {
 
-        if(component.definition && !(component.definition instanceof URI)) {
-	    if (component.definition.components.length === 0) return segments
+        if (component.definition && !(component.definition instanceof URI)) {
+            if (component.definition.components.length === 0) return segments
 
-             var segment = getDisplayListSegment(component.definition)
+            var segment = getDisplayListSegment(component.definition, config)
 
-             if(segment.sequence.length > 0) {
-		 if (segments.filter(function(e) { return e.name == segment.name; }).length == 0) {
-                     segments.push(segment)
-		 }
-	     }
-	     segments = recurseGetDisplayList(component.definition,segments)
-         }
+            if (segment.sequence.length > 0) {
+                if (segments.filter(function (e) { return e.name == segment.name; }).length == 0) {
+                    segments.push(segment)
+                }
+            }
+            segments = recurseGetDisplayList(component.definition, segments, config)
+        }
 
     })
     return segments
 }
 
-function getDisplayListSegment(componentDefinition) {
+function getDisplayListSegment(componentDefinition, config) {
 
-    var displayName = componentDefinition.displayId 
+    var displayName = componentDefinition.displayId
     if (componentDefinition.name != '' && componentDefinition.name != componentDefinition.displayId) {
-	displayName += ' ('+componentDefinition.name+')'
+        displayName += ' (' + componentDefinition.name + ')'
     }
 
-    if (componentDefinition.sequenceAnnotations.length===0) {
+    if (componentDefinition.sequenceAnnotations.length === 0) {
 
-	var glyph = 'user-defined'
-	var name = componentDefinition.name != ''?componentDefinition.name:componentDefinition.displayId
-	var roles = componentDefinition.roles
+        var glyph = 'user-defined'
+        var name = componentDefinition.name != '' ? componentDefinition.name : componentDefinition.displayId
+        var roles = componentDefinition.roles
 
-	var tooltip = 'Component\n'
-	if (componentDefinition.displayId) tooltip += 'Identifier: ' + componentDefinition.displayId + '\n'
-	if (componentDefinition.name) tooltip += 'Name: ' + componentDefinition.name + '\n'
-	if (componentDefinition.description) tooltip += 'Description: ' + componentDefinition.description + '\n'
-		
-	roles.forEach((role) => {
+        var tooltip = 'Component\n'
+        if (componentDefinition.displayId) tooltip += 'Identifier: ' + componentDefinition.displayId + '\n'
+        if (componentDefinition.name) tooltip += 'Name: ' + componentDefinition.name + '\n'
+        if (componentDefinition.description) tooltip += 'Description: ' + componentDefinition.description + '\n'
+
+        roles.forEach((role) => {
 
             var so = (role + '').match(/SO.([0-9]+)/g)
-	    
-            if(!so || !so.length)
-		return
+
+            if (!so || !so.length)
+                return
 
             var soCode = so[0].split('_').join(':')
 
             var glyphType = soToGlyphType(soCode)
 
-            if(glyphType)
-		glyph = glyphType
+            if (glyphType)
+                glyph = glyphType
 
-	    tooltip += 'Role: ' + role
-	})
+            tooltip += 'Role: ' + role
+        })
 
-	return {
-	    name: displayName,
-	    sequence: [{
+        if (config) {
+            uriToReturn = '/' + component.definition.uri.toString().replace(config.get('databasePrefix'), '');
+        } else {
+            uriToReturn = '/' + component.definition.uri.toString();
+        }
+
+        return {
+            name: displayName,
+            sequence: [{
                 strand: "positive",
                 type: glyph,
                 id: componentDefinition.uri + '',
                 name: name,
-                uri: '/'  + componentDefinition.uri.toString(),
-		tooltip: tooltip
-	    }]
-	}
+                uri: uriToReturn,
+                tooltip: tooltip
+            }]
+        }
     }
 
     return {
@@ -97,38 +103,35 @@ function getDisplayListSegment(componentDefinition) {
 
             var glyph = 'user-defined'
 
-            var name = sequenceAnnotation.name!=''?sequenceAnnotation.name:sequenceAnnotation.displayId
+            var name = sequenceAnnotation.name != '' ? sequenceAnnotation.name : sequenceAnnotation.displayId
             var roles = sequenceAnnotation.roles
 
             var uri = ''
-	    var tooltip = ''
+            var tooltip = ''
 
-
-	  if(sequenceAnnotation.component && sequenceAnnotation.component.definition) {
+            if (sequenceAnnotation.component) {
 
                 var component = sequenceAnnotation.component
-		tooltip = 'Component\n'
-                if(!(component.definition instanceof URI)) {
+                tooltip = 'Component\n'
+                if (component.definition && !(component.definition instanceof URI)) {
 
                     roles = roles.concat(component.definition.roles)
 
-                    name = component.definition.name!=''?component.definition.name:component.definition.displayId
-
-                    uri = '/'  + component.definition.uri.toString()
-
-		    if (component.definition.displayId) tooltip += 'Identifier: ' + component.definition.displayId + '\n'
-		    if (component.definition.name) tooltip += 'Name: ' + component.definition.name + '\n'
-		    if (component.definition.description) tooltip += 'Description: ' + component.definition.description + '\n'
+                    name = component.definition.name != '' ? component.definition.name : component.definition.displayId
+                    uri = uriToReturn
+                    if (component.definition.displayId) tooltip += 'Identifier: ' + component.definition.displayId + '\n'
+                    if (component.definition.name) tooltip += 'Name: ' + component.definition.name + '\n'
+                    if (component.definition.description) tooltip += 'Description: ' + component.definition.description + '\n'
                 } else {
-			uri = component.definition.toString();
-		}
+                    uri = componentDefinition.toString()
+                }
 
             } else {
-		tooltip = 'Feature\n'
-		if (sequenceAnnotation.displayId) tooltip += 'Identifier: ' + sequenceAnnotation.displayId + '\n'
-		if (sequenceAnnotation.name) tooltip += 'Name: ' + sequenceAnnotation.name + '\n'
-		if (sequenceAnnotation.description) tooltip += 'Description: ' + sequenceAnnotation.description + '\n'
-	    }
+                tooltip = 'Feature\n'
+                if (sequenceAnnotation.displayId) tooltip += 'Identifier: ' + sequenceAnnotation.displayId + '\n'
+                if (sequenceAnnotation.name) tooltip += 'Name: ' + sequenceAnnotation.name + '\n'
+                if (sequenceAnnotation.description) tooltip += 'Description: ' + sequenceAnnotation.description + '\n'
+            }
 
             roles.forEach((role) => {
 
@@ -136,16 +139,16 @@ function getDisplayListSegment(componentDefinition) {
                 var igemFeaturePrefix = 'http://synbiohub.org/terms/igem/feature/'
                 var soPrefix = 'http://identifiers.org/so/'
 
-                if(role.toString().indexOf(igemPartPrefix) === 0) {
+                if (role.toString().indexOf(igemPartPrefix) === 0) {
 
                     tooltip += 'iGEM Part Type: ' + role.toString().slice(igemPartPrefix.length) + '\n'
 
-                } else if(role.toString().indexOf(igemFeaturePrefix) === 0) {
+                } else if (role.toString().indexOf(igemFeaturePrefix) === 0) {
 
                     tooltip += 'iGEM Feature Type: ' + role.toString().slice(igemFeaturePrefix.length) + '\n'
 
-                } else if(role.toString().indexOf(soPrefix) === 0) {
-		    
+                } else if (role.toString().indexOf(soPrefix) === 0) {
+
                     var soTerm = role.toString().slice(soPrefix.length).split('_').join(':')
                     tooltip += 'Role: ' + sbolmeta.sequenceOntology[soTerm].name + '\n'
 
@@ -153,30 +156,30 @@ function getDisplayListSegment(componentDefinition) {
 
                 var so = (role + '').match(/SO.([0-9]+)/g)
 
-                if(!so || !so.length)
+                if (!so || !so.length)
                     return
 
                 var soCode = so[0].split('_').join(':')
 
                 var glyphType = soToGlyphType(soCode)
 
-                if(glyphType)
+                if (glyphType)
                     glyph = glyphType
             })
-	    
-	    sequenceAnnotation.ranges.forEach((range) => {
-		if (range.orientation) tooltip += 'Orientation: ' + range.orientation.toString().replace('http://sbols.org/v2#','') + '\n'
-		tooltip += range.start + '..' + range.end + '\n'
-	    })
-	    
-	    sequenceAnnotation.cuts.forEach((cut) => {
-		if (cut.orientation) tooltip += 'Orientation: ' + cut.orientation.toString().replace('http://sbols.org/v2#','') + '\n'
-		tooltip += cut.at + '^' + cut.at + '\n'
-	    })
-	    
-	    sequenceAnnotation.genericLocations.forEach((genericLocation) => {
-		if (genericLocation.orientation) tooltip += 'Orientation: ' + genericLocation.orientation.toString().replace('http://sbols.org/v2#','') + '\n'
-	    })
+
+            sequenceAnnotation.ranges.forEach((range) => {
+                if (range.orientation) tooltip += 'Orientation: ' + range.orientation.toString().replace('http://sbols.org/v2#', '') + '\n'
+                tooltip += range.start + '..' + range.end + '\n'
+            })
+
+            sequenceAnnotation.cuts.forEach((cut) => {
+                if (cut.orientation) tooltip += 'Orientation: ' + cut.orientation.toString().replace('http://sbols.org/v2#', '') + '\n'
+                tooltip += cut.at + '^' + cut.at + '\n'
+            })
+
+            sequenceAnnotation.genericLocations.forEach((genericLocation) => {
+                if (genericLocation.orientation) tooltip += 'Orientation: ' + genericLocation.orientation.toString().replace('http://sbols.org/v2#', '') + '\n'
+            })
 
             return {
                 strand: "positive",
@@ -184,7 +187,7 @@ function getDisplayListSegment(componentDefinition) {
                 id: sequenceAnnotation.uri + '',
                 name: name,
                 uri: uri,
-		tooltip: tooltip
+                tooltip: tooltip
             }
         })
     }
@@ -194,26 +197,26 @@ function sortedSequenceAnnotations(componentDefinition) {
 
     return componentDefinition.sequenceAnnotations.sort((a, b) => {
 
-	if (a.ranges.length > 0 && b.ranges.length > 0) {
-	    if (start(a)===start(b)) {
-		return end(a) - end(b)
-	    } else {
-		return start(a) - start(b)
-	    }
-	} else if (a.component && b.component) {
-	    return position(componentDefinition, a.component) - position(componentDefinition, b.component)
-	}
-	return start(a) - start(b)
+        if (a.ranges.length > 0 && b.ranges.length > 0) {
+            if (start(a) === start(b)) {
+                return end(a) - end(b)
+            } else {
+                return start(a) - start(b)
+            }
+        } else if (a.component && b.component) {
+            return position(componentDefinition, a.component) - position(componentDefinition, b.component)
+        }
+        return start(a) - start(b)
 
     })
 
     function start(sequenceAnnotation) {
 
-	var minStart = sequenceAnnotation.ranges.length > 0 ? sequenceAnnotation.ranges[0].start : 0
-	for (var i = 0; i < sequenceAnnotation.ranges.length; i++) {
-	    if (sequenceAnnotation.ranges[i].start < minStart)
-		minStart = sequenceAnnotation.ranges[i].start
-	}
+        var minStart = sequenceAnnotation.ranges.length > 0 ? sequenceAnnotation.ranges[0].start : 0
+        for (var i = 0; i < sequenceAnnotation.ranges.length; i++) {
+            if (sequenceAnnotation.ranges[i].start < minStart)
+                minStart = sequenceAnnotation.ranges[i].start
+        }
         return minStart
 
     }
@@ -221,30 +224,30 @@ function sortedSequenceAnnotations(componentDefinition) {
     function end(sequenceAnnotation) {
 
 
-	var maxEnd = sequenceAnnotation.ranges.length > 0 ? sequenceAnnotation.ranges[0].end : 0
-	for (var i = 0; i < sequenceAnnotation.ranges.length; i++) {
-	    if (sequenceAnnotation.ranges[i].end < maxEnd)
-		maxEnd = sequenceAnnotation.ranges[i].end
-	}
+        var maxEnd = sequenceAnnotation.ranges.length > 0 ? sequenceAnnotation.ranges[0].end : 0
+        for (var i = 0; i < sequenceAnnotation.ranges.length; i++) {
+            if (sequenceAnnotation.ranges[i].end < maxEnd)
+                maxEnd = sequenceAnnotation.ranges[i].end
+        }
         return maxEnd
 
     }
 
     // TODO: note that cycle of sequenceConstraints creates infinite loop
-    function position(componentDefinition,component) {
+    function position(componentDefinition, component) {
 
-	var curPos = 0
-	componentDefinition.sequenceConstraints.forEach((sequenceConstraint) => {
-	    sequenceConstraint.link()
-	    if (sequenceConstraint.restriction.toString()==='http://sbols.org/v2#precedes') {
-		if (sequenceConstraint.object.uri.toString()===component.uri.toString()) {
-		    var subPos = position(componentDefinition,sequenceConstraint.subject)
-		    if (subPos+1 > curPos)
-			curPos = subPos + 1
-		}
-	    }
-	})
-	return curPos
+        var curPos = 0
+        componentDefinition.sequenceConstraints.forEach((sequenceConstraint) => {
+            sequenceConstraint.link()
+            if (sequenceConstraint.restriction.toString() === 'http://sbols.org/v2#precedes') {
+                if (sequenceConstraint.object.uri.toString() === component.uri.toString()) {
+                    var subPos = position(componentDefinition, sequenceConstraint.subject)
+                    if (subPos + 1 > curPos)
+                        curPos = subPos + 1
+                }
+            }
+        })
+        return curPos
 
     }
 
