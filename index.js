39a065c601467467e8c382869696987ba20508c1
Zach Zundel <zach.zundel@utah.edu>
2017-05-09 11:02:55 +0100

    Removed SVG export

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 
-var SVG = require('./lib/svg'),
-    Design = require('./lib/design'),
+var Design = require('./lib/design'),
     Vec2 = require('./lib/geom/vec2'),
     Rect = require('./lib/geom/rect'),
     Matrix = require('./lib/geom/matrix'),
     getDisplayList = require('./lib/getDisplayList')
 
 module.exports = {
-    SVG: SVG,
     Design: Design,
     Vec2: Vec2,
     Rect: Rect,
