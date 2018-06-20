13e6208bd70019b32cd008bb67d1b81a256d5eb2
Stephanie Gomez <stephanie.a.gomez95@gmail.com>
2018-06-20 13:48:09 -0400

    Added small molecule

--- font/sbolv/small_molecule.js
+++ font/sbolv/small_molecule.js
@@ -2,102 +2,79 @@
 var Vec2 = require('../../lib/geom/vec2')
 var Rect = require('../../lib/geom/rect')
 
-
-function createGeometry(boxSize) {
-
-    return {
-      leftEyeLeft: Vec2(boxSize.x * 0.25, boxSize.x * 0.4),
-      leftEyeRight: Vec2(boxSize.x * 0.3, boxSize.x * 0.4),
-      rightEyeleft: Vec2(boxSize.x * 0.75, boxSize.x * 0.4),
-      rightEyeright: Vec2(boxSize.x * 0.8, boxSize.x * 0.4),
-      left: Vec2(boxSize.x * 0.3, boxSize.x * 0.8),
-      right: Vec2(boxSize.x * 0.7, boxSize.x * 0.8),
-      noseTop: Vec2(boxSize.x * 0.5, boxSize.x * 0.5),
-      noseBottom: Vec2(boxSize.x * 0.5, boxSize.x * 0.7),    
-   };
+function createGeometry(boxSize){
+//Coordinates for rectangle
+return{
+    topLeft: Vec2(boxSize.x/2,0),
+    bottomLeft: Vec2(boxSize.x/2,boxSize.y),              //Larger outer box
+    bottomRight: Vec2(boxSize.x * 2,boxSize.y),
+    topRight: Vec2(boxSize.x * 2,0),
+  };
 }
-
 function renderGlyph(design, glyphObject, boxSize) {
 
     var geom = createGeometry(boxSize);
-    
-
-    var path = [
-
-        'M' + Vec2.toPathString(geom.left),
-        'L' + Vec2.toPathString(geom.right),
-         
+    var path1 = [
+      'M' + Vec2.toPathString(geom.topLeft),
+      'L' + Vec2.toPathString(geom.bottomLeft),
+      'L' + Vec2.toPathString(geom.bottomRight),
+      'L' + Vec2.toPathString(geom.topRight),
+      'L' + Vec2.toPathString(geom.topLeft)
     ].join('');
 
     var path2 = [
+      'M' + Vec2.toPathString(geom.topLeft),
+      'L' + Vec2.toPathString(geom.topRight),
+      'M' + Vec2.toPathString(geom.bottomLeft),
+      'L' + Vec2.toPathString(geom.bottomRight),
 
-        'M' + Vec2.toPathString(geom.leftEyeLeft),
-        'L' + Vec2.toPathString(geom.leftEyeRight),
-        'M' + Vec2.toPathString(geom.rightEyeleft),
-        'L' + Vec2.toPathString(geom.rightEyeright),
-         
     ].join('');
 
-    var path3 = [
+  var glyph1 = design.surface.path(path1);
+  var glyph2 = design.surface.path(path2);
 
-        'M' + Vec2.toPathString(geom.noseTop),
-        'L' + Vec2.toPathString(geom.noseBottom)
-        
-    ].join('');
+  var glyph3 = design.surface.circle(boxSize.x);
+  var glyph4 = design.surface.circle(boxSize.x);
+  var group = design.surface.group();
 
+  glyph1.attr('stroke','dark gray');
+  glyph1.attr('fill', glyphObject.color || '#787878');
+  glyph1.attr('stroke-width', glyphObject.thickness ||'2px');
+  glyph1.attr('stroke-linejoin', 'square');
 
-    var glyph1 = design.surface.circle(
-        boxSize.x, boxSize.x);
+  glyph2.attr('stroke','black');
+  glyph2.attr('fill', glyphObject.color || '#787878');
+  glyph2.attr('stroke-width', glyphObject.thickness ||'2px');
+  glyph2.attr('stroke-linejoin', 'square');
 
-    glyph1.attr('stroke', 'black');
-    glyph1.attr('fill', glyphObject.color || '#cee');
-    glyph1.attr('stroke-width', glyphObject.thickness || '5px');
+  glyph3.attr('stroke','black');
+  glyph3.attr('fill', glyphObject.color || '#787878');
+  glyph3.attr('stroke-width', glyphObject.thickness ||'2px');
+  glyph3.attr('stroke-linejoin', 'round');
+  glyph3.attr({ cx: boxSize.x*2, cy: boxSize.y/2 })
 
-    var glyph2 = design.surface.path(path);
-   
-    glyph2.attr('stroke', 'red');
-    glyph2.attr('fill', 'none');
-    glyph2.attr('stroke-width', glyphObject.thickness || '3px');
-    glyph2.attr('stroke-linecap', 'round');
+  glyph4.attr('stroke','black');
+  glyph4.attr('fill', glyphObject.color || '#787878');
+  glyph4.attr('stroke-width', glyphObject.thickness ||'2px');
+  glyph4.attr('stroke-linejoin', 'round');
 
-    var glyph3 = design.surface.path(path2);
-   
-    glyph3.attr('stroke', 'green');
-    glyph3.attr('fill', 'none');
-    glyph3.attr('stroke-width', glyphObject.thickness || '3px');
-    glyph3.attr('stroke-linecap', 'round');
+  group.add(glyph3);
+  group.add(glyph4);
+  group.add(glyph1);
+  group.add(glyph2);
 
-    var glyph4 = design.surface.path(path3);
-   
-    glyph4.attr('stroke', 'purple');
-    glyph4.attr('fill', 'none');
-    glyph4.attr('stroke-width', glyphObject.thickness || '3px');
-    glyph4.attr('stroke-linecap', 'round');
-  
+  if(glyphObject.uri){
+      group.attr('data-uri', glyphObject.uri)
+    }
 
-    var group = design.surface.group();
-    group.add(glyph1);
-    group.add(glyph2);
-    group.add(glyph3);
-    group.add(glyph4);
+  return {
+      glyph: group,
+      backboneOffset: boxSize.y
+  };
+  }
 
-     boundingBox.attr('fill-opacity', 0);
+  module.exports = {
 
-    if(glyphObject.uri)
-        boundingBox.attr('data-uri', glyphObject.uri);
-    group.add(boundingBox);
+  render: renderGlyph
 
-    return {
-        glyph: group,
-        backboneOffset: boxSize.y
-    };
-}
-
-module.exports = {
-
-    render: renderGlyph
-
-};
-
-
-
+  };
