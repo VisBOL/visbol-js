9ac6d05702d0dc14701d2e93e34dbbf2dd473714
benjhatch <benjamin.hat4@gmail.com>
2020-01-29 15:23:41 -0700

    label for non-coding-rna-gene.js has been fixed (it was too high, moved lower to prevent arrow overlapping

--- font/sbolv/non-coding-rna-gene.js
+++ font/sbolv/non-coding-rna-gene.js
@@ -8,7 +8,7 @@ function createGeometry(boxSize) {
 
       var lengthX = pointB.x - pointA.x;
       var lengthY = pointB.y - pointA.y;
-      
+
       return {
           length: Math.sqrt(Math.pow(lengthX, 2) + Math.pow(lengthY, 2)),
           angle: Math.atan2(lengthY, lengthX)
@@ -17,32 +17,35 @@ function createGeometry(boxSize) {
     }
 
     function createControlPoint(current, previous, next) {
-         
+
          const smoothing = 0.25;
          var p = (previous === null) ? current : previous;
          var n = (next === null) ? current: next;
-          
+
          var line = createTangentLine(n,p);
-          
+
          return {
              x: current.x + (Math.cos(line.angle + Math.PI) * line.length * smoothing),
              y: current.y + (Math.sin(line.angle + Math.PI) * line.length * smoothing)
-         };  
+         };
     }
+
+    var bodyCurveProportion = 0.7;
+    var heightDecrease = 0.4;
 
     var topLeft = Vec2(0.0, boxSize.y * 0.8);
     var bottomLeft = Vec2(0.0, boxSize.y);
     var bottomRight = Vec2(boxSize.x, boxSize.y);
-    var topRight = Vec2(boxSize.x, boxSize.y * 0.9);
-        
-    //coordinate of pick
-    var rightMostPick = Vec2(boxSize.x * 0.88, boxSize.y * 0.7);
-    var right = Vec2(boxSize.x * 0.7, boxSize.y * 0.9);
-    var middlePick = Vec2(boxSize.x * 0.5, boxSize.y * 0.7);
-    var middle = Vec2(boxSize.x * 0.3, boxSize.y * 0.9);
-    var leftMostPick = Vec2(boxSize.x * 0.1, boxSize.y * 0.7);
+    var topRight = Vec2(boxSize.x, boxSize.y * bodyCurveProportion);
 
-    //coordiantes of control ponits 
+    //coordinate of pick
+    var rightMostPick = Vec2(boxSize.x * 0.88, boxSize.y * heightDecrease);
+    var right = Vec2(boxSize.x * 0.7, boxSize.y * bodyCurveProportion);
+    var middlePick = Vec2(boxSize.x * 0.5, boxSize.y * heightDecrease);
+    var middle = Vec2(boxSize.x * 0.3, boxSize.y * bodyCurveProportion);
+    var leftMostPick = Vec2(boxSize.x * 0.1, boxSize.y * heightDecrease);
+
+    //coordiantes of control ponits
     var controlPoint1 = createControlPoint(topRight, rightMostPick, null);
     var controlPoint2 = createControlPoint(rightMostPick, right, topRight);
     var controlPoint3 = createControlPoint(right, middlePick, rightMostPick);
@@ -50,13 +53,13 @@ function createGeometry(boxSize) {
     var controlPoint5 = createControlPoint(middle, leftMostPick, middlePick);
     var controlPoint6 = createControlPoint(leftMostPick, topLeft, middle);
     var controlPoint7 = createControlPoint(topLeft, null, leftMostPick);
-    
+
     return {
        topLeft: topLeft,
        bottomLeft: bottomLeft,
        bottomRight: bottomRight,
        topRight: topRight,
-        
+
        //coordinate of pick
        rightMostPick: rightMostPick,
        right: right,
@@ -64,23 +67,23 @@ function createGeometry(boxSize) {
        middle: middle,
        leftMostPick: leftMostPick,
 
-      //coordiantes of control ponits 
-      controlPoint1: controlPoint1, 
-      controlPoint2: controlPoint2, 
+      //coordiantes of control ponits
+      controlPoint1: controlPoint1,
+      controlPoint2: controlPoint2,
       controlPoint3: controlPoint3,
-      controlPoint4: controlPoint4, 
-      controlPoint5: controlPoint5, 
-      controlPoint6: controlPoint6, 
+      controlPoint4: controlPoint4,
+      controlPoint5: controlPoint5,
+      controlPoint6: controlPoint6,
       controlPoint7: controlPoint7
 
-       
+
   };
 }
 
 function renderGlyph(design, glyphObject, boxSize) {
 
     var geom = createGeometry(boxSize);
-     
+
 
     var path = [
 
@@ -94,7 +97,7 @@ function renderGlyph(design, glyphObject, boxSize) {
         'S' + Vec2.toPathString(geom.controlPoint5) + ' ' + Vec2.toPathString(geom.middle),
         'S' + Vec2.toPathString(geom.controlPoint6) + ' ' + Vec2.toPathString(geom.leftMostPick),
         'S' + Vec2.toPathString(geom.controlPoint7) + ' ' + Vec2.toPathString(geom.topLeft)
-      
+
      ].join('');
 
     var glyph = design.surface.path(path);
@@ -105,7 +108,7 @@ function renderGlyph(design, glyphObject, boxSize) {
     glyph.attr('stroke-linecap', 'round');
     glyph.attr('fill', glyphObject.color || '#A9A9A9');
 
-    
+
     group.add(glyph);
 
     boundingBox = design.surface.rect(boxSize.x, boxSize.y);
@@ -126,11 +129,8 @@ module.exports = {
 
     render: renderGlyph,
 
-   /* insets: {
-        top: 0.2
-    }*/
+    insets: {
+        top: 0.48
+    }
 
 };
-
-
-
