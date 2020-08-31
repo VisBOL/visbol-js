9e1d134393c33eee6d03379e23d237a3149a4b2d
benjhatch <benjamin.hat4@gmail.com>
2020-08-30 20:39:38 -0700

    added getInteractionList to original VisBOL

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ var Design = require('./lib/design'),
     Vec2 = require('./lib/geom/vec2'),
     Rect = require('./lib/geom/rect'),
     Matrix = require('./lib/geom/matrix'),
-    getDisplayList = require('./lib/getDisplayList')
+    getDisplayList = require('./lib/getDisplayList'),
+    getInteractionList = require('./lib/getInteractionList')
 
 module.exports = {
     Design: Design,
     Vec2: Vec2,
     Rect: Rect,
     Matrix: Matrix,
-    getDisplayList: getDisplayList
+    getDisplayList: getDisplayList,
+    getInteractionList: getInteractionList
 };
 
 
