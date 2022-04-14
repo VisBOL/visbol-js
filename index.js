2bf77ff9046e138e375dc6614a20aee8e5b38735
Marco Amato <linediconsine@gmail.com>
2022-04-14 12:13:46 +0200

    getDisplayListJson provide a Json with information about the sbol components

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
+const getDisplayListJson = require('./lib/getDisplayListJson');
 const getDisplayList = require('./lib/getDisplayList');
 const getInteractionList = require('./lib/getInteractionList');
 
 module.exports = {
+    getDisplayListJson: getDisplayListJson,
     getDisplayList: getDisplayList,
     getInteractionList: getInteractionList
 };
